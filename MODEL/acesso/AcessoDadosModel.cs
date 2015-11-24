48839645e0b427914057b2f7f7c7a199feaf02e8
felipepcoelho <felipe.pereira.coelho@gmail.com>
2015-11-24 00:27:35 -0300

    Atualização de modelos

--- MODEL/acesso/AcessoDadosModel.cs
+++ MODEL/acesso/AcessoDadosModel.cs
@@ -11,38 +11,15 @@ namespace MODEL.acesso
     public class AcessoDadosModel:IModel
     {
         #region Atributos
-        private int id;
-        private string login;
-        private string senha;
-        public UsuarioCadastroModel usuario;
-        #endregion
-
-        #region Encapsulamento
-        public int ID {
-            get { return this.id; }
-            set { this.id = value;}
-        }
-        public string Login {
-            get { return this.login; }
-            set { 
-                if (value.Length > 30)
-                   throw new Exception("O Tamanho da palavra ultrapassa 30 caracters!");
-                this.login = value;
-            }
-        }
-        public string Senha {
-            get { return this.senha; }
-             set { 
-                if (value.Length > 30)
-                   throw new Exception("O Tamanho da palavra ultrapassa 30 caracters!");
-                this.senha = value;
-            }
-        }
+        public int ID {get;set;}
+        public string Login { get; set; }
+        public string Senha {get;set;}
+        public UsuarioCadastroModel Usuario{get;set;};
         #endregion
 
         #region Construtores
         public AcessoDadosModel() {
-            this.usuario = new UsuarioCadastroModel();
+            this.Usuario = new UsuarioCadastroModel();
         }
 
         public AcessoDadosModel(AcessoDadosModel model)
@@ -50,7 +27,7 @@ namespace MODEL.acesso
             this.ID = model.ID;
             this.Login = model.Login;
             this.Senha = model.Senha;
-            this.usuario = model.usuario;
+            this.Usuario = model.Usuario;
         }
         #endregion
 
--- MODEL/usuario/UsuarioCadastroModel.cs
+++ MODEL/usuario/UsuarioCadastroModel.cs
@@ -10,54 +10,12 @@ namespace MODEL.usuario
     public class UsuarioCadastroModel:IModel
     {
         #region Atributos
-        private int id;
-        private string nome;
-        private int idade;
-        private DateTime dataNascimento;
-        private string cpf;
-        private string rg;
-        #endregion
-
-        #region Encapsulamento
-        public string Nome
-        {
-            get { return nome; }
-            set { nome = value; }
-        }
-
-
-        public int Idade
-        {
-            get { return idade; }
-            set { idade = value; }
-        }
-
-
-        public DateTime DataNascimento
-        {
-            get { return dataNascimento; }
-            set { dataNascimento = value; }
-        }
-
-
-        public string Cpf
-        {
-            get { return cpf; }
-            set { cpf = value; }
-        }
-
-
-        public string Rg
-        {
-            get { return rg; }
-            set { rg = value; }
-        }
-
-        public int ID {
-            get { return this.id; }
-            set { this.id = value; }
-        }
-
+        public int ID { get; set; }
+        public string Nome {get;set;}
+        public int Idade {get;set;}
+        public DateTime DataNascimento{get;set;}
+        public string Cpf {get;set;}
+        public string Rg{get;set;}
         #endregion
 
         #region Construtores
